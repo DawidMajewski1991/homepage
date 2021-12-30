3e74be5ae2940bdf42fc32515af04be211e5dd95
Dawid Majewski <dawidmajewski1991@gmail.com>
2021-12-30 22:36:52 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Hej hej!');
+
+// alert('Siema!');
+
+// const firstName = 'John';
+// const age = 20;
+// alert(firstName + ' ' + age);
+// console.log(firstName + ' ' + age);
+// console.log(age);
+
+// console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+// console.log('Nazywam się '+firstName+ ' i mam '+age+ ' lat.😀');
+
+// alert(`Nazywam się ${firstName} i mam ${age} lat.😀`);
+
+// console.log(`Witaj. Ja jestem ${firstName} i mam ${age} lat.😀`);
