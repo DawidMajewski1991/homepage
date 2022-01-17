bf52fde15318810c3294f16a2e6ac2809b4b6454
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-01-17 12:44:40 +0100

    feat: pseudo-classes, pseudo-eplements, hover and octive on links, star.js

---
+++ js/star.js
@@ -0,0 +1,23 @@
+{
+    "diameter": 120000,
+    "isOperating": true,
+    "levels": 357,
+    "name": "Death Star",
+    "population": 10000,
+    "crew": {
+        "role": "Captain",
+        "name": "Campbell",
+    }
+}
+
+const newStar = {
+    "diameter": 120000,
+    "isOperating": true,
+    "levels": 357,
+    "name": "Death Star",
+    "population": 10000,
+    "crew": {
+        "role": "Captain",
+        "name": "Campbell",
+    }
+}
